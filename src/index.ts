0f59f6cd587d81dcf64ed0c49fc665f2decda3a4
FlavioFj18 <fuxiflavio86@gmail.com>
2026-08-18 15:32:59 +0100

    feat: add axios dependency and implement getPost function to fetch post data

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
 #!/usr/bin/env node
-console.log("Hello from weather api!");
+import axios from "axios";
+//import type { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+
+// const response = await axios.get(
+//   "https://jsonplaceholder.typicode.com/posts/1", {
+//   timeout: 5000,}
+// );
+
+// console.log(response.data);
+
+// type Post = {
+//   userId: number;
+//   id: number;
+//   title: string;
+//   body: string;
+// };
+
+// const response = await axios.get<Post>("https://jsonplaceholder.typicode.com/posts/1");
+
+// console.log(response.data.title); // TypeScript knows this is a string
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const getPost = async (id: number): Promise<Post> => {
+  const response = await axios.get<Post>(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  return response.data;
+};
+
+console.log(await getPost(1))
